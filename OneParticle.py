fixed sign error

OneParticle.py
@@ -5,15 +5,13 @@ from plotting import *
 
 B1=np.array([0,0,B0])
 theta=np.pi/9
-print(theta)
 S1init=np.array([np.sin(theta),0,np.cos(theta)])
-print(S1init)
 T=100
 H1=Hj1(B1,mu)
 
-S1Heun, t1Heun = Heun(h,S1init,T,mu,gamma,alfa,H1,oneSpin=True)
-np.save("S1Heun",S1Heun)
-np.save("t1Heun",t1Heun)
+# S1Heun, t1Heun = Heun(h,S1init,T,mu,gamma,alfa,H1,oneSpin=True)
+# np.save("S1Heun",S1Heun)
+# np.save("t1Heun",t1Heun)
 
 #plot1Spin(S1,len(t1)-1)
 
@@ -29,12 +27,24 @@ np.save("t1Heun",t1Heun)
 # np.save("S1Analytic",S1Analytic)
 # np.save("t1Analytic",t1Analytic)
 
+# print("calculations done")
 
-# compareSolutions("S1Heun.npy","S1Euler.npy","S1Analytic.npy","t1Heun.npy","ComparisonPlot")
+# #compareSolutions("S1Heun.npy","S1Euler.npy","S1Analytic.npy","t1Heun.npy","ComparisonPlot")
 
 # HeunError, HeunhList=errorVsStepsize(Heun,S1init,T,mu,gamma,alfa,H1)
+# print("yo")
 # EulerError, EulerhList=errorVsStepsize(Euler,S1init,T,mu,gamma,alfa,H1)
 
 # plotErrorVsStepsize(HeunError,HeunhList,EulerError, EulerhList,"ErrorPlot")
+# print("done")
 
 #%lprun -f plotErrorVsStepsize x = plotErrorVsStepsize(HeunError,HeunhList,EulerError, EulerhList,"test")
+
+
+"""With damping"""
+dampedAlfa=0.05
+S1HeunDamped, t1HeunDamped = Heun(h,S1init,T,mu,gamma,dampedAlfa,H1,oneSpin=True)
+np.save("S1HeunDamped",S1HeunDamped)
+np.save("t1HeunDamped",t1HeunDamped)
+
+plotXYZ1Spin(S1HeunDamped,t1HeunDamped)

animation.py
@@ -8,7 +8,7 @@ X=0
 Y=1
 Z=2
 
-S=np.load("S1Heun.npy") 
+S=np.load("S1HeunDamped.npy") 
 
 fig = plt.figure()
 ax = fig.gca(projection='3d')

functions.py
@@ -1,5 +1,6 @@
 import numpy as np
 from constants import *
+from numba import jit
 
 """Initialization"""
 #initialize spin array
@@ -10,8 +11,9 @@ from constants import *
 #B is the magnetic field
 
 #only 1 particle
+#@jit(nopython = True)
 def Hj1(B,mu): 
-    return -mu*B 
+    return mu*B 
 
 #multiple particles
 """
@@ -24,15 +26,18 @@ def H(S,J,B,mu):
 """
 
 """function for f(y,t) in Heun method"""
+@jit(nopython = True)
 def f(S,mu,gamma,alfa,H): #H and S are arrays of vectors
+    #print("alfa",alfa)
     ScrossH=np.cross(S,H) #cross product of S with H
     return (-gamma/(np.abs(mu)*(1+alfa*alfa)))*(ScrossH+np.cross((alfa*S),ScrossH))
 
 
 """Heun method"""
 
-
+#@jit(nopython = True)
 def Heun(h,S0,T,mu,gamma,alfa,Hj,oneSpin=False): #T is the total time
+    
     if oneSpin:
         N=1
     else: 
@@ -47,7 +52,7 @@ def Heun(h,S0,T,mu,gamma,alfa,Hj,oneSpin=False): #T is the total time
 
 """Euler method"""
 
-
+#@jit(nopython = True)
 def Euler(h,S0,T,mu,gamma,alfa,Hj,oneSpin=False):
     if oneSpin:
         N=1
@@ -63,7 +68,7 @@ def Euler(h,S0,T,mu,gamma,alfa,Hj,oneSpin=False):
 """Error estimate"""
 #Analytic solution for 1 particle, with magn.field in z-direction, and the spin in the xy-plane
 
-
+@jit(nopython = True)
 def analyticSol1Particle(h,T,S0,Hj):
     N=len(S0)
     t=np.arange(0,T,h)
@@ -81,6 +86,7 @@ def analyticSol1Particle(h,T,S0,Hj):
 
 #calculates total error: the difference between two arrays after a given time (last elements of arrays)
 #NB written for only 1 spin
+#@jit(nopython = True)
 def calculateError(Snum, Sanalytic): 
     #print("YO",Sanalytic)
     N=len(Snum)-1
@@ -89,11 +95,13 @@ def calculateError(Snum, Sanalytic):
     return totalError
 
 #Calculates error for given method as a function of stepsize
+#@jit(nopython = True)
 def errorVsStepsize(methodFunction,S0,T,mu,gamma,alfa,Hj): #n is the number of elements in hList
     hList=np.linspace(0.00001,0.1,5)
     errors=np.zeros(len(hList))
     i=0
     for hi in hList:
+        print(hi)
         Sanalytic,tana=analyticSol1Particle(hi,T,S0,Hj)
         Si,ti=methodFunction(hi,S0,T,mu,gamma,alfa,Hj,oneSpin=True)
         errors[i]=calculateError(Si,Sanalytic)

plotting.py
@@ -36,13 +36,17 @@ def plot1Spin(S,ti):
 
 
 
-def plotXandYfor1Spin(S,t):
+def plotXYZ1Spin(S,t):
     x=S[:,:,X]
     y=S[:,:,Y]
-    length=np.sqrt(x*x+y*y)
-    plt.plot(t,x)
-    plt.plot(t,y)
-    plt.plot(t,length,color="red")
+    z=S[:,:,Z]
+    length=np.sqrt(x*x+y*y+z*z)
+    fig,ax=plt.subplots(1,1)
+    ax.plot(t,x)
+    ax.plot(t,y)
+    ax.plot(t,z)
+    ax.plot(t,length,color="red")
+    fig.savefig("1spin")
     plt.show()
 
 
@@ -70,12 +74,13 @@ def compareSolutions(SHeundFile,SEulerFile,SAnalyticFile,tFile,filename):
 
 
 def plotErrorVsStepsize(HeunError,HeunSteps,EulerError,EulerSteps,filename):
+    print("OK")
     fig, ax=plt.subplots(1,1)
     ax.set_xlabel("Step size")
     ax.set_ylabel("Error")
     fig.suptitle("Error as a function of step size")
-    ax.plot(HeunSteps,HeunError,label="Heun")
-    ax.plot(EulerSteps,EulerError,label="Euler")
+    ax.plot(np.log(HeunSteps),np.log(HeunError),label="Heun")
+    ax.plot(np.log(EulerSteps),np.log(EulerError),label="Euler")
     fig.legend()
     fig.savefig(filename)
     plt.show()